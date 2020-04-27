bbbd7784c83dc366b141b782840283f5c6791c9b
konstantin2508 <moyapochta82@mail.ru>
2020-04-27 11:33:48 +0200

    adding движение (влево/вправо) корабля

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,8 +22,9 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
-            self._check_events()
-            self._update_screen()
+            self._check_events()        # Обработка событий
+            self.ship.update()          # Изменение положения корабля
+            self._update_screen()       # Обновление изображения на экране
 
     # Вспомогательный метод-работает во внутренней реализации класса
     def _check_events(self):
@@ -31,6 +32,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Переместить корабль вправо
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Переместить корабль влево
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран"""
