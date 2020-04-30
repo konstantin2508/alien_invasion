52be7a5ad2d536171e81fe33f0ca1c86d51e880c
konstantin2508 <moyapochta82@mail.ru>
2020-04-30 16:36:32 +0200

    Добавление bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -23,12 +24,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
             self._check_events()        # Обработка событий
             self.ship.update()          # Изменение положения корабля
+            self.bullets.update()       # Изменение положения снаряда
             self._update_screen()       # Обновление изображения на экране
 
     # Вспомогательный метод-работает во внутренней реализации класса
@@ -53,6 +56,9 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             # Завершение игры при нажатии Q
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            # Выпустить снаряд
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш"""
@@ -61,11 +67,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
