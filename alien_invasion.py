4772c83bf1ced64d1313e203cfb736526fe7a54d
konstantin2508 <moyapochta82@mail.ru>
2020-05-02 13:10:54 +0200

    Создание флота пришельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,11 +87,34 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_alien(self, alien_number, row_number):
+        """Создание пришельца и размещение его в ряду"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
     def _create_fleet(self):
         """ Создание флота пришельцев"""
-        # Создание пришельца
+        # Создание пришельца и вычисление кол-ва пришельцев в ряду
+        # Интервал между соседними пришельцами равен ширине пришельца
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        """Определяет кол-во рядов, помещающихся на экран"""
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Создание флота пришельцев
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран"""
