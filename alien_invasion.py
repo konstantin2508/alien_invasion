43f9a9167907288f47b56a11db33fd46293a9cbf
konstantin2508 <moyapochta82@mail.ru>
2020-05-08 16:19:16 +0200

    Создание движение пришельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,6 +36,7 @@ class AlienInvasion:
             self._check_events()        # Обработка событий
             self.ship.update()          # Изменение положения корабля
             self._update_bullets()      # Изменение положения снаряда и удаление старых
+            self._update_aliens()       # Изменение положения пришельцев
             self._update_screen()       # Обновление изображения на экране
 
     # Вспомогательный метод-работает во внутренней реализации класса
@@ -87,6 +88,14 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """
+        Проверяет, достиг ли флот края экрана,
+        с последующим обновлением позиций всех пришельцев во флоте
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_alien(self, alien_number, row_number):
         """Создание пришельца и размещение его в ряду"""
         alien = Alien(self)
@@ -95,6 +104,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет напрвление флота"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _create_fleet(self):
         """ Создание флота пришельцев"""
--- settings.py
+++ settings.py
@@ -15,3 +15,9 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3            # Допустимое кол-во снарядов
+
+        # Настройки пришельца
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево
+        self.fleet_direction = 1
